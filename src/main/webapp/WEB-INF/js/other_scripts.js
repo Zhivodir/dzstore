f96f8a7f0309a25c6feea03a0a562e0e8b01b2b7
zhivodir <dzhivchik@gmail.com>
2017-07-11 10:24:08 +0300

    Выделение с помощью Ctrl и Shift

--- src/main/webapp/WEB-INF/js/other_scripts.js
+++ src/main/webapp/WEB-INF/js/other_scripts.js
@@ -2,26 +2,58 @@
  * Created by User on 21.06.2017.
  */
 
-$(document).ready(function () {
-    $('.record_table tr').click(function (event) {
-        if (event.target.type !== 'checkbox') {
-            $(':checkbox', this).trigger('click');
-        }
-    });
-});
-
-
 onload = function() { tree("tree", "/ajax/load_tree_of_catalog") }
+
 
 document.oncontextmenu = function (){return false};
 
 
-$('#myTable input[type="checkbox"]').click(function(){
-    if($(this).prop('checked'))
-        $(this).parent().parent().addClass('selected');
-    else
-        $(this).parent().parent().removeClass('selected');
+var tr = $('#myTable .choise_field'),
+    selection = {
+        single: function(el) {
+            tr.not(el).removeClass(this.cl);
+            this.ctrl(el);
+        },
+        shift: function(el) {
+
+            if (typeof this.last !== 'number') {
+                return this.single(el);
+            }
+            var till = $(el).index(this.slcr),
+                from = this.last;
+            if (from > till) till = [from, from = till][0];
+            tr.not(tr.eq(this.last)).removeClass(this.cl);
+            tr.slice(from, till).add(el).addClass(this.cl);
+        },
+        ctrl: function(el) {
+            if($(el).hasClass('selected')){
+                $(el).removeClass(this.cl);
+            }else{
+                $(el).addClass(this.cl);
+            }
+            this.last = $(el).index(this.slcr);
+            console.log(this.last);
+        },
+        slcr: '#myTable .choise_field',
+        cl: 'selected',
+        last: null
+    };
+
+
+tr.on('click', function(e) {
+     method = !e.shiftKey && !e.ctrlKey ? 'single' : (e.shiftKey ? 'shift' : 'ctrl');
+    selection[method](this);
+    $('#myTable tr').each(function(indx, el) {
+        $("input:checkbox").removeAttr("checked");
+    })
+    $('#myTable tr.selected').each(function(indx, el) {
+        var inp = $("input:checkbox", el)[0];
+        event.target != inp && (inp.checked = !inp.checked)
+    })
 });
+
+
+
 
 jQuery(function($) {
     $('.folder_href').click(function() {
