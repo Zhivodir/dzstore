7e9b3c13ebf965967bbcf7728acc82df27f92f77
zhivodir <dzhivchik@gmail.com>
2018-05-31 15:08:00 +0300

    Reduction getShareReceivers(String shareFor) method's code.

--- src/main/java/com/gmail/dzhivchik/dao/Impl/UserDAOImpl.java
+++ src/main/java/com/gmail/dzhivchik/dao/Impl/UserDAOImpl.java
@@ -12,10 +12,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
- * Created by User on 09.02.2017.
- */
-
 @Repository
 public class UserDAOImpl implements UserDAO {
     @PersistenceContext
@@ -53,51 +49,22 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
-    public void delete(User user) {
-
-    }
+    public void delete(User user) {}
 
     @Override
     public List<User> getShareReceivers(String shareFor) {
-        Query query = entityManager.
-                createQuery(preparationShareRequest(shareFor), User.class);
-        return (List<User>) query.getResultList();
-    }
-
-
-    public String preparationShareRequest(String shareFor) {
         String[] tempListForShare = shareFor.split(",");
         List<String> forEmail = new ArrayList<>();
         List<String> forLogin = new ArrayList<>();
         for (String receiver : tempListForShare) {
             receiver = receiver.trim();
-            if (receiver.contains("@")) {
-                forEmail.add(receiver);
-            } else {
-                forLogin.add(receiver);
-            }
+            if (receiver.contains("@")) forEmail.add(receiver);
+            else forLogin.add(receiver);
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append("SELECT u FROM User u WHERE ");
-        if (forEmail.size() != 0) {
-            for (int i = 0; i < forEmail.size(); i++) {
-                sb.append("u.email = '" + forEmail.get(i) + "' ");
-                if (i != forEmail.size() - 1) {
-                    sb.append("OR ");
-                }
-            }
-        }
-        if (forLogin.size() != 0) {
-            if (forEmail.size() != 0) {
-                sb.append("OR ");
-            }
-            for (int j = 0; j < forLogin.size(); j++) {
-                sb.append("u.login = '" + forLogin.get(j) + "' ");
-                if (j != forLogin.size() - 1) {
-                    sb.append("OR ");
-                }
-            }
-        }
-        return sb.toString();
+        Query query = entityManager.createQuery("SELECT u FROM User u " +
+                "WHERE u.email IN :forEmail OR u.login IN :forLogin", User.class);
+        query.setParameter("forEmail", forEmail);
+        query.setParameter("forLogin", forLogin);
+        return (List<User>) query.getResultList();
     }
 }
