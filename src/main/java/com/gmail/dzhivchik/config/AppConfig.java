df780a6c9a2f96c0523c84bc9f8319337fb8c30c
zhivodir <dzhivchik@gmail.com>
2019-05-24 11:40:27 +0300

    Fix error in heroku

--- src/main/java/com/gmail/dzhivchik/config/AppConfig.java
+++ src/main/java/com/gmail/dzhivchik/config/AppConfig.java
@@ -1,9 +1,11 @@
 package com.gmail.dzhivchik.config;
 
 import com.gmail.dzhivchik.service.Impl.UserDetailsServiceImpl;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -13,14 +15,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Locale;
 
 @Configuration
 @ComponentScan("com.gmail.dzhivchik")
@@ -94,7 +99,6 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         ds.setUrl("jdbc:mysql://eu-cdbr-west-01.cleardb.com:3306/heroku_2411027723b6162?useUnicode=true&characterEncoding=UTF-8");
         ds.setUsername("b37e2c921f7603");
         ds.setPassword("51d552f6");
-//test
         return ds;
     }
 
@@ -103,4 +107,19 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         return new UserDetailsServiceImpl();
     }
 
+    /** I18n **/
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:lang/texts");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(new Locale("en"));
+        return resolver;
+    }
 }
