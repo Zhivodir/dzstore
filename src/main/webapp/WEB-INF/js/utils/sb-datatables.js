eaec5f587a91d1f546cebc1622f831e3153ab06d
zhivodir <dzhivchik@gmail.com>
2019-10-09 14:50:22 +0300

    New changelist

--- src/main/webapp/WEB-INF/js/utils/sb-datatables.js
+++ src/main/webapp/WEB-INF/js/utils/sb-datatables.js
@@ -98,15 +98,6 @@ function showUsersTableStateInfo(hasUsersWithAccess) {
     }
 }
 
-// function formatSize(length) {
-//     var i = 0, type = ['б', 'Кб', 'Мб', 'Гб', 'Тб', 'Пб'];
-//     while ((length / 1000 | 0) && i < type.length - 1) {
-//         length /= 1024;
-//         i++;
-//     }
-//     return length.toFixed(2) + ' ' + type[i];
-// }
-
 function formatSize(length) {
     var i = 0, type = ['б', 'Кб', 'Мб', 'Гб'];
     while ((length / 1000 | 0) && i < type.length - 1) {
